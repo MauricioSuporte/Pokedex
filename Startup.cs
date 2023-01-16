c947cb2caf5156e11a7a8e8b5a3473945306dfe1
ACERTO\mauricio.souza <mauricio.souza@meuacerto.com.br>
2023-01-16 11:46:15 -0300

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,31 @@
+﻿namespace Pokedex;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+    private readonly IServiceCollection _services;
+
+    public Startup(WebApplicationBuilder builder)
+    {
+        _configuration = builder.Configuration;
+        _services = builder.Services;
+    }
+
+    public void ConfigureServices()
+    {
+        _services.AddControllers();
+        _services.AddEndpointsApiExplorer();
+        _services.AddSwaggerGen();
+    }
+
+    public void ConfigureApplication(IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(options => options.MapControllers());
+    }
+}
